b84e04d6f73f49935742962b694949d36a6a801f
Jack <jackwakeham82@gmail.com>
2019-09-04 16:59:52 +0100

    Course update

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssimport = require('postcss-import');
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssimport = require('postcss-import'),
+    mixins = require('postcss-mixins');
 
 gulp.task('styles', () => {
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssimport,cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
+        .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
