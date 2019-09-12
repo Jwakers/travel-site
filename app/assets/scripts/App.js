1e0ef5b3becd96d843aea563b949f10b72735b34
Jack <jackwakeham82@gmail.com>
2019-09-12 16:59:53 +0100

    Sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll('.feature-item', "85%", true);
 new RevealOnScroll('.testimonial');
+new StickyHeader();
