5dbb26f8c51edab8bd6f57a8f7688c9b0a328957
Jack <jackwakeham82@gmail.com>
2019-09-13 12:03:26 +0100

    Slow scrolling

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,11 +1,20 @@
 import $ from 'jquery';
-import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints.js'
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints.js';
+import smoothScroll from 'jquery-smooth-scroll';
 
 export default class StickyHeader {
     constructor() {
         this.siteHeader = $('.site-header');
         this.triggerElement = $('.large-hero__title');
         this.createWaypoint();
+        this.pageSection = $('.page-section');
+        this.headerLinks = $('.primary-nav a');
+        this.createPageSectionWaypoint();
+        this.addSmoothScrolling();
+    }
+
+    addSmoothScrolling() {
+        this.headerLinks.smoothScroll();
     }
 
     createWaypoint() {
@@ -15,6 +24,35 @@ export default class StickyHeader {
             handler: function() {
                 that.siteHeader.toggleClass("site-header--dark");
             }
+        })
+    }
+
+    createPageSectionWaypoint() {
+        var that = this;
+        this.pageSection.each(function() {
+            var currentEl = this;
+            new Waypoint({
+                element: currentEl,
+                handler: function(direction) {
+                    if (direction === "down") {
+                        var headerLink = this.element.getAttribute('data-matching-link');
+                        that.headerLinks.removeClass('is-current-link');
+                        $(headerLink).addClass('is-current-link');
+                    }
+                },
+                offset: "18%"
+            });
+            new Waypoint({
+                element: currentEl,
+                handler: function(direction) {
+                    if (direction === "up") {
+                        var headerLink = this.element.getAttribute('data-matching-link');
+                        that.headerLinks.removeClass('is-current-link');
+                        $(headerLink).addClass('is-current-link');
+                    }
+                },
+                offset: "-40%"
+            })
         })
     }
 }
